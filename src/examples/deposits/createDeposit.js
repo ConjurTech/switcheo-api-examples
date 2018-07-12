33c9bf4fe553c5f98101994bd47eccae681d2d6a
John Wong <john.wong@switcheo.network>
2018-07-12 15:35:40 +0800

    Standardise createDeposit example

--- src/examples/deposits/createDeposit.js
+++ src/examples/deposits/createDeposit.js
@@ -2,14 +2,12 @@ const { getTimestamp, signParams } = require('../../utils')
 const { API_URL, CONTRACT_HASH } = require('../../config')
 const api = require('../../api')
 
-function createDeposit ({ contractHash, blockchain, address,
-    assetID, amount, privateKey }) {
-    const timestamp = getTimestamp()
-    const signableParams = { blockchain, contractHash, assetID, amount, timestamp }
-    const signature = signParams(signableParams, privateKey)
-    const apiParams = { ...signableParams, address, signature, timestamp }
-      console.log(apiParams)
-    return api.post(API_URL + '/deposits', apiParams)
-  };
+function createDeposit ({ blockchain, address, assetID, amount, privateKey }) {
+  const signableParams = { blockchain, assetID, amount, timestamp: getTimestamp(),
+                           contractHash: CONTRACT_HASH }
+  const signature = signParams(signableParams, privateKey)
+  const apiParams = { ...signableParams, address, signature }
+  return api.post(API_URL + '/deposits', apiParams)
+};
 
 module.exports = { createDeposit }
--- src/examples/deposits/exampleParams/exampleDepositParams.js
+++ src/examples/deposits/exampleParams/exampleDepositParams.js
@@ -1,10 +1,8 @@
 const user = require('../../../user')
 const { toNeoAssetAmount } = require('../../../utils')
-const { CONTRACT_HASH } = require('../../../config')
 
 module.exports = {
   depositParams: {
-    contractHash: CONTRACT_HASH,
     blockchain: 'neo',
     address: user.address,
     assetID: 'ab38352559b8b203bde5fddfa0b07d8b2525e132',
