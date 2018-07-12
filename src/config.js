2cb9fbbca94fc4aea9a92d4a041a292a0e5ffc3c
PC <spclight@gmail.com>
2018-07-12 17:14:29 +0800

    Update contract hash

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   API_URL: 'https://test-api.switcheo.network/v2',
-  CONTRACT_HASH: '9e9b631b416645bd7c356454d5b895c3df051eb9'
+  CONTRACT_HASH: 'eed0d2e14b0027f5f30ade45f2b23dc57dd54ad2'
 }
 
 // TODO: Remove this
