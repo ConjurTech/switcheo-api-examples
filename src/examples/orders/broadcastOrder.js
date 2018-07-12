937098e3514e2663c5a2be3e9c39fcc89a4c55ee
John Wong <john.wong@switcheo.network>
2018-07-12 12:21:24 +0800

    Add broadcastOrder example

---
+++ src/examples/orders/broadcastOrder.js
@@ -0,0 +1,22 @@
+const { signTransaction } = require('../../utils')
+const { API_URL } = require('../../config')
+const api = require('../../api')
+
+function signArray(array, privateKey) {
+  return array.reduce((map, item) => {
+    map[item.id] = signTransaction(item.txn, privateKey)
+    return map
+  }, {})
+}
+
+function broadcastOrder({ order, privateKey }) {
+  const { fills, makes } = order
+  const signatures = {
+    fills: signArray(order.fills, privateKey),
+    makes: signArray(order.makes, privateKey)
+  }
+  const url = `${API_URL}/orders/${order.id}/broadcast`
+  return api.post(url, { signatures })
+}
+
+module.exports = { broadcastOrder }
